f566442b4783aba7b9fec0820c442839a4d6f7d3
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2025-01-16 10:03:48 -0300

    Add files via upload

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -22,14 +22,59 @@
 			l->prim = novo;
 		}
 		void lst_imprime (Lista* l){
+			int i=0;
 			ListaNo* aux;
 			for(aux = l->prim; aux != NULL; aux = aux->prox){
-				printf(" %d ", aux->info);
+				i++;
+				printf("\nitem %d: %d\n ",i, aux->info);
+				
 			}
+		}
+//		int lst_vazia(Lista *l){
+//				if(l->prim == NULL){
+//					return 1;
+//				}
+//				else {
+//			     	return 0;
+//			         }
+//			}
+		
+		int lst_vazia(Lista *l){  //mesma coisa da lista passada. 
+			return l->prim == NULL;
+		}
+		
+		int lst_pertence(Lista *l, int v){
+			ListaNo* p;
+//			int *opcao;
+		for(p = l->prim; p != NULL; p = p->prox){
+				if(p->info == v){
+					return 1;
+				}
+			}
+			return 0;
+		}
+		void lst_retira(Lista *l, int v){
+			ListaNo *ant = NULL;
+			ListaNo *p = l->prim;
+			while(p!= NULL && p->info != v){
+				ant = p;
+				p = p->prox;
+			}
+			if(p != NULL){
+				if(ant == NULL){
+					l->prim = p->prox;
+				}
+				else {
+					ant->prox = p->prox;
+				}
+				
+			free(p);
+			}  
+		
 		}
 		int main(){
 			int i;
-			
+		
 			Lista* l = lst_cria();
 			
 			if (l==NULL){
@@ -37,12 +82,25 @@
 			} else {
 				printf("Lista alocada!");
 			} 
+			
+			printf("A lista esta vazia?: %d", lst_vazia(l));
 			lst_insere(l, 10);
 			lst_insere(l, 20);
-			printf("\nPrimeiro: %d\n", l->prim->info);
-			
+			lst_insere(l, 30);
+			lst_insere(l, 40);
+			lst_insere(l, 50);
+			lst_insere(l, 60);
+//			printf("\nPrimeiro: %d\n", l->prim->info);
 			lst_imprime (l);
+			printf("\nA lista esta vazia?: %d", lst_vazia(l));	
 			
+			lst_retira(l, 10);
+			lst_retira(l, 20);	
+			lst_retira(l, 30);
+			lst_imprime(l);
+//		do {
+//		    lst_pertence(l, opcao);
+//		}while(lst_pertence != opcao);
 			return 0;
 		}
 		
